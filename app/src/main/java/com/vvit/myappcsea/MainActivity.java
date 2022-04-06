81f405e9cdc7c8b26e34d0dbf8bfeecf495bf4df
Sriharsha1103 <sriharsha1103@gmail.com>
2022-04-06 10:14:36 +0530

    Getting Data from  web

--- app/src/main/java/com/vvit/myappcsea/MainActivity.java
+++ app/src/main/java/com/vvit/myappcsea/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new MyAsyncTask().execute(NetworkUtils.buildURL("popular",true));
+        new MyAsyncTask().execute(NetworkUtils.buildURL("top_rated",true));
     }
 }
